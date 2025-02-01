580c8b63800eecdadcbcb5b5506a63c0973e985a
SaffronGamer <saffrongamer167@gmail.com>
2025-01-31 18:36:04 -0700

    attempted to add reading from file features

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,20 @@
+use std::{fs::File, io::Read, path::Path};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn load_file(path: String) -> String {
+    let mut buffer = String::new();
+    match File::open(Path::new(&path)).unwrap().read_to_string(&mut buffer) {
+        Ok(_) => buffer,
+        Err(err) => err.to_string(),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -3,16 +3,30 @@ const { invoke } = window.__TAURI__.core;
 let greetInputEl;
 let greetMsgEl;
 
-async function greet() {
+async function load_file() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  greetMsgEl.textContent = await invoke("load_file", { num: Number(greetInputEl.value) });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+  greetInputEl = document.querySelector("#greet-input")
+  greetMsgEl = document.querySelector("#greet-msg")
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+    e.preventDefault()
+    let reader = new FileReader()
+    let file = greetInputEl.files[0] // Access the file from the input element
+
+    reader.onload = function (e) {
+      let file_output = e.target.result; // Set the file_output variable
+      document.getElementById("file_output").innerHTML = file_output;
+      console.log(file_output)
+    }
+    console.log(file_output)
+    
+    if (file) {
+      reader.readAsText(file) // Read the file as text
+    } else {
+      console.log("No file selected")
+    }
+  })
+})
